9db3884a7facbab8160a2634e77499643886c170
Tyrthurey <tyrthurey@gmail.com>
2025-07-11 18:23:30 +0300

    Fix(discord): prevent rpc connection from blocking launch

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -11,36 +11,42 @@ let client
 let activity
 
 exports.initRPC = function (genSettings, servSettings, initialDetails = Lang.queryJS('discord.waiting')) {
-    client = new Client({ transport: 'ipc' })
+    try {
+        client = new Client({ transport: 'ipc' })
 
-    activity = {
-        details: initialDetails,
-        state: Lang.queryJS('discord.state', { shortId: servSettings.shortId }),
-        largeImageKey: servSettings.largeImageKey,
-        largeImageText: servSettings.largeImageText,
-        smallImageKey: genSettings.smallImageKey,
-        smallImageText: genSettings.smallImageText,
-        startTimestamp: new Date().getTime(),
-        instance: false,
-    }
-
-    client.on('ready', () => {
-        logger.info('Discord RPC Connected')
-        client.setActivity(activity)
-    })
-
-    client.login({ clientId: genSettings.clientId }).catch(error => {
-        if (error.message.includes('ENOENT')) {
-            logger.info('Unable to initialize Discord Rich Presence, no client detected.')
-        } else {
-            logger.info('Unable to initialize Discord Rich Presence: ' + error.message, error)
+        activity = {
+            details: initialDetails,
+            state: Lang.queryJS('discord.state', { shortId: servSettings.shortId }),
+            largeImageKey: servSettings.largeImageKey,
+            largeImageText: servSettings.largeImageText,
+            smallImageKey: genSettings.smallImageKey,
+            smallImageText: genSettings.smallImageText,
+            startTimestamp: new Date().getTime(),
+            instance: false,
         }
-    })
+
+        client.on('ready', () => {
+            logger.info('Discord RPC Connected')
+            client.setActivity(activity)
+        })
+
+        client.login({ clientId: genSettings.clientId }).catch(error => {
+            if (error.message.includes('ENOENT')) {
+                logger.info('Unable to initialize Discord Rich Presence, no client detected.')
+            } else {
+                logger.info('Unable to initialize Discord Rich Presence: ' + error.message, error)
+            }
+        })
+    } catch (error) {
+        logger.info('Unable to initialize Discord Rich Presence: ' + error.message, error)
+    }
 }
 
 exports.updateDetails = function (details) {
-    activity.details = details
-    client.setActivity(activity)
+    if (client) {
+        activity.details = details
+        client.setActivity(activity)
+    }
 }
 
 exports.shutdownRPC = function () {
